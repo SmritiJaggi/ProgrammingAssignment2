4f862cf2bc3f6a6235e579dbd4ab3bf9724ea1f5
SmritiJaggi <smritijaggi@gmail.com>
2015-01-23 12:26:16 -0800

    Revert "Cached Inverse of a Matrix"
    This reverts commit 2ba0d2c596087bbfa35cb316b5b60996674cd5e6.

--- cachematrix.R
+++ cachematrix.R
@@ -1,42 +1,15 @@
-#name       : makeCacheMatrix 
-#description: Get a vector of the matrix
-#i/p param  : matrix x
-#o/p param  : numeric vector
+## Put comments here that give an overall description of what your
+## functions do
 
-makeCacheMatrixVector <- function(x = matrix()) {
+## Write a short comment describing this function
 
-	i <- NULL                                   #initially setting the inverse of the matrix to NULL
-	set <- function(y) {
-		x <<- y                             #setting the value of inverse of the matrix from cache
-		i <<- NULL
-	}
-	get <- function() x                         #getting the value of the matrix x
-	setinverse <- function(solve) i <<- solve   #setting the value of the inverse in the cache
-	getinverse <- function() i                  #getting the value of the inverse that was set earlier
-	list(set = set, get = get,
-	     setinverse = setinverse,
-	     getinverse = getinverse)               #list of functions in makeCacheMatrix
+makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-#name       : cacheSolve 
-#description: Get the inverse of matrix x using getinverse and setinverse functions
-#i/p param  : matrix x
-#o/p param  : inverse of the matrix
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-	
-	i <- NULL
-
-	i <- x$getinverse                          #getting the inverse of the matrix 
-	if(!is.null(i)){                           #If i is not NULL, we have retrieved cached value for the inverse
-		message("getting cached inverse")
-		return(i)                            
-	}
-	data <- x$get()                            #getting the matrix value of x as inverse is not present in cache
-	i<- solve(data,...)                        #getting inverse value for the matrx using solve() function
-	x$setinverse(i)                            #setting the value of the inverse in cache
-
-	i                                          #returning the value of the inverse of the matrix
+        ## Return a matrix that is the inverse of 'x'
 }
